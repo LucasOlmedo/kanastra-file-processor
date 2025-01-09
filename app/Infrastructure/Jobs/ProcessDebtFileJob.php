e647b9874bd2c74ebee942932e6de24472d9b66c
Lucas Olmedo Silva <lcsolmedosilva@gmail.com>
2025-01-09 17:09:06 -0300

    creates chunk file service and dispatch job

--- app/Infrastructure/Jobs/ProcessDebtFileJob.php
+++ app/Infrastructure/Jobs/ProcessDebtFileJob.php
@@ -4,6 +4,7 @@ namespace App\Infrastructure\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 class ProcessDebtFileJob implements ShouldQueue
 {
@@ -17,5 +18,8 @@ class ProcessDebtFileJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(): void {}
+    public function handle(): void
+    {
+        Log::warning('ProcessDebtFileJob');
+    }
 }
--- app/Infrastructure/Services/ProcessDebtFileService.php
+++ app/Infrastructure/Services/ProcessDebtFileService.php
@@ -2,11 +2,33 @@
 
 namespace App\Infrastructure\Services;
 
-use Illuminate\Support\Facades\Storage;
+use SplFileObject;
 
 class ProcessDebtFileService
 {
-    const CHUNK_SIZE = 1000;
+    const CHUNK_SIZE = 5000;
 
-    public function readAndChunkDebtFile(string $filePath, int $chunkSize = self::CHUNK_SIZE) {}
+    public function readAndChunkDebtFile(string $filePath)
+    {
+        $fileObject = new SplFileObject($filePath, 'r');
+        $fileObject->setFlags(SplFileObject::READ_CSV);
+        $fileObject->fgetcsv();
+
+        while (!$fileObject->eof()) {
+            $chunk = $this->chunkCsv($fileObject);
+            if ($chunk)
+                yield $chunk;
+        }
+    }
+
+    private function chunkCsv(SplFileObject $fileObject): array
+    {
+        $chunk = [];
+        while (count($chunk) < self::CHUNK_SIZE && !$fileObject->eof()) {
+            $line = $fileObject->fgetcsv();
+            if ($line !== [null])
+                $chunk[] = $line;
+        }
+        return $chunk;
+    }
 }
