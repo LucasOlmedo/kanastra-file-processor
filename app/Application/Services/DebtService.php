009bec7099bf2bd7131888f6382d8eb4d7933af9
Lucas Olmedo Silva <lcsolmedosilva@gmail.com>
2025-01-10 08:23:51 -0300

    refactors debt service

--- app/Application/Services/DebtService.php
+++ app/Application/Services/DebtService.php
@@ -2,25 +2,20 @@
 
 namespace App\Application\Services;
 
-use App\Application\UseCases\ProcessDebtUseCase;
 use App\Infrastructure\Jobs\ProcessChunkDebtDataJob;
 use App\Infrastructure\Services\ProcessDebtFileService;
 
 class DebtService
 {
     public function __construct(
-        private ProcessDebtFileService $processDebtFileService,
-        private ProcessDebtUseCase $processDebtUseCase
+        private ProcessDebtFileService $processDebtFileService
     ) {}
 
     public function processFile(string $filePath)
     {
         $chunkFile = $this->processDebtFileService->readAndChunkDebtFile($filePath);
         foreach ($chunkFile as $chunk) {
-            foreach ($chunk as $line) {
-                $this->processDebtUseCase->execute($line);
-            }
-            // ProcessChunkDebtDataJob::dispatch($chunk);
+            ProcessChunkDebtDataJob::dispatch($chunk);
         }
     }
 }
